2da5f0878719c04e7cee7a731a95a62937c72f40
ioannya <ivank8037@gmail.com>
2025-05-04 14:05:42 +0300

    Update Green_4.cs

--- Green_4.cs
+++ Green_4.cs
@@ -1,67 +1,58 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab_8
 {
     public class Green_4 : Green
     {
         private string[] _output;
-        private string _input;
-        public string[] Output => _output;
+        public string[] Output => _output?.ToArray();
 
         public Green_4(string input) : base(input)
         {
-            _input = input;
             _output = null;
         }
 
         public override void Review()
         {
-            if (Input == null || Input.Length == 0)
+            if (string.IsNullOrWhiteSpace(Input))
             {
                 _output = new string[0];
                 return;
             }
-            string[] surnames = Input.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-            string[] surnamesNorm = new string[surnames.Length];
 
-            for (int i = 0; i < surnames.Length; i++)
-            {
-                surnamesNorm[i] = surnames[i].Trim();
-            }
+            string[] surnames = Input.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
 
-            for (int i = 1; i < surnamesNorm.Length; i++)
+            for (int i = 1; i < surnames.Length; i++)
             {
-                string key = surnamesNorm[i];
+                string key = surnames[i];
                 int j = i - 1;
-                while (j >= 0 && CompareStrings(surnamesNorm[j], key) > 0)
+                while (j >= 0 && CompareStrings(surnames[j], key) > 0)
                 {
-                    surnamesNorm[j + 1] = surnamesNorm[j];
+                    surnames[j + 1] = surnames[j];
                     j--;
                 }
-                surnamesNorm[j + 1] = key;
+                surnames[j + 1] = key;
             }
-            _output = surnamesNorm;
+
+            _output = surnames;
         }
+
         private int CompareStrings(string a, string b)
         {
-            int minlen = Math.Min(a.Length, b.Length);
-            for (int i = 0; i < minlen; i++)
+            int minLen = Math.Min(a.Length, b.Length);
+            for (int i = 0; i < minLen; i++)
             {
-                if (a[i] != b[i]) return Math.Sign(a[i] - b[i]);
+                if (a[i] != b[i]) return a[i] - b[i];
             }
-            return Math.Sign(a.Length - b.Length);
+            return a.Length - b.Length;
         }
+
         public override string ToString()
         {
-            if (_output == null || _output.Length == 0)
-            {
-                return string.Empty;
-            }
-            return string.Join(Environment.NewLine, Output);
+            return _output == null || _output.Length == 0
+                ? string.Empty
+                : string.Join(Environment.NewLine, _output);
         }
     }
 }
